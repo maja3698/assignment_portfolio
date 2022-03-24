ad5bb9278afd9e8d31c1e8720c295360f6e209b6
Maja Berendsen <maja498h@stud.kea.dk>
2022-03-24 23:54:55 +0100

    Reload

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
+let menu = document.getElementById("menu");
+let nav = document.getElementById("navigation");
+let exit = document.getElementById("exit");
+
+let desktop = window.matchMedia("(min-width: 1234px)")
+
+const header = document.querySelector(".leftheader")
+
+menu.addEventListener("click", function (e) {
+  nav.classList.add("nav-transition");
+  // e.preventDefault();
+});
+
+exit.addEventListener("click", function (e) {
+  nav.classList.remove("nav-transition");
+  // e.preventDefault();
+});
+
+nav.addEventListener("click", function (e) {
+  nav.classList.remove("nav-transition");
+});
 
 
-let menu = document.getElementById('menu');
-    let nav = document.getElementById('navigation');
-    let exit = document.getElementById('exit');
 
-    menu.addEventListener('click', function (e) {
-        nav.classList.add('nav-transition');
-        // e.preventDefault();
-    });
+const keyframes = [
+  {
+    height: "30vh",
+    easing: "ease-in",
+  },
+  {
+    height: "100vh",
+    easing: "ease-in",
+  },
+];
 
-    exit.addEventListener('click', function (e) {
-        nav.classList.remove('nav-transition');
-        // e.preventDefault();
-    });
+const properties = {
+  duration: 750,
+  delay: 2,
+  iterations: 1,
+};
 
-    nav.addEventListener('click', function (e) {
-        nav.classList.remove('nav-transition');
-    });
 
-    function openNav() {
-        document.getElementById("slideNav").style.width = "1000px";
-    }
+if (desktop.matches){
+  header.animate(keyframes, properties);
 
-    function closeNav() {
-        document.getElementById("slideNav").style.width = "0";
-    }
+}
+function openNav() {
+  document.getElementById("slideNav").style.width = "1000px";
+}
 
-    
+function closeNav() {
+  document.getElementById("slideNav").style.width = "0";
+}
+
+
+
+
+
+
+
